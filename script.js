c49f31e8b1fcad8535cb7515d95e54fa40b0076b
Josue Alamo <josuealamo95@gmail.com>
2022-07-03 13:48:56 -0500

    Added add, subtract, multiply, divide and operator functions to script and tested them.

---
+++ script.js
@@ -0,0 +1,22 @@
+const add = function(a, b) {
+    return a + b;
+};
+
+const subtract = function(a, b) {
+    return a - b;
+};
+
+const multiply = function(a, b) {
+    return a * b;
+};
+
+const divide = function(a, b) {
+    return a / b;
+};
+
+const operate = (operator, a, b) => {
+    if (operator == '+') {return add(a, b)};
+    if (operator == '-') {return subtract(a, b)};
+    if (operator == '*') {return multiply(a, b)};
+    if (operator == '/') {return divide(a, b)};
+};
